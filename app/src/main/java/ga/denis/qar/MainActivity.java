997b49af2d93849b7e9fc13375c21b2343013611
Denis Masek <denis.masek@student.gyarab.cz>
2022-03-24 13:08:07 +0100

    Initial commit

---
+++ app/src/main/java/ga/denis/qar/MainActivity.java
@@ -0,0 +1,66 @@
+package ga.denis.qar;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+
+import java.io.ByteArrayOutputStream;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr);
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this, QRAR.class);
+                        intent.putExtra("text", result.getText());
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/ga/denis/qar/QRAR.java
@@ -0,0 +1,125 @@
+package ga.denis.qar;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.core.TrackingState;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class QRAR extends AppCompatActivity implements
+        FragmentOnAttachListener,
+        BaseArFragment.OnSessionConfigurationListener {
+
+    private final List<CompletableFuture<Void>> futures = new ArrayList<>();
+    private ArFragment arFragment;
+    private AugmentedImageDatabase database;
+    boolean qrDetected = false;
+    Bitmap qr;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().addFragmentOnAttachListener(this);
+
+        intent = getIntent();
+        QRGEncoder qrgEncoder = new QRGEncoder(intent.getExtras().getString("text"), null, QRGContents.Type.TEXT, 1024);
+        qrgEncoder.setColorBlack(Color.BLACK);
+        qrgEncoder.setColorWhite(Color.WHITE);
+        qr = Bitmap.createBitmap(qrgEncoder.getBitmap(), 50, 50, 924, 924);
+        ImageView imageView = findViewById(R.id.imageView2);
+        imageView.setImageBitmap(qr);
+
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
+        }
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+        if (fragment.getId() == R.id.arFragment) {
+            arFragment = (ArFragment) fragment;
+            arFragment.setOnSessionConfigurationListener(this);
+        }
+    }
+
+    @Override
+    public void onSessionConfiguration(Session session, Config config) {
+        config.setPlaneFindingMode(Config.PlaneFindingMode.DISABLED);
+
+        database = new AugmentedImageDatabase(session);
+
+        database.addImage("qr", qr);
+
+        config.setAugmentedImageDatabase(database);
+
+        arFragment.setOnAugmentedImageUpdateListener(this::onAugmentedImageTrackingUpdate);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        futures.forEach(future -> {
+            if (!future.isDone())
+                future.cancel(true);
+        });
+    }
+
+    public void onAugmentedImageTrackingUpdate(AugmentedImage augmentedImage) {
+        if (augmentedImage.getTrackingState() == TrackingState.TRACKING
+                && augmentedImage.getTrackingMethod() == AugmentedImage.TrackingMethod.FULL_TRACKING) {
+
+            AnchorNode anchorNode = new AnchorNode(augmentedImage.createAnchor(augmentedImage.getCenterPose()));
+
+            if (!qrDetected && augmentedImage.getName().equals("qr")) {
+                qrDetected = true;
+                arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+                futures.add(ModelRenderable.builder()
+                        .setSource(this, Uri.parse(intent.getExtras().getString("text")))
+                        .setIsFilamentGltf(true)
+                        .build()
+                        .thenAccept(rabbitModel -> {
+                            TransformableNode modelNode = new TransformableNode(arFragment.getTransformationSystem());
+                            modelNode.setRenderable(rabbitModel);
+                            anchorNode.addChild(modelNode);
+                        })
+                        .exceptionally(
+                                throwable -> {
+                                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
+                                    return null;
+                                }));
+            }
+        }
+    }
+}
